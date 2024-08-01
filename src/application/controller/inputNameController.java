e14bed0407d58afa23ac0c71cc55045b4725a27f
Alex Becker <alex.becker@hs-osnabrueck.de>
2024-08-01 19:53:55 +0200

    added menus

---
+++ src/application/controller/inputNameController.java
@@ -0,0 +1,15 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class inputNameController {
+
+    @FXML
+    private Button buttonSave;
+
+    @FXML
+    private TextField nameInputField;
+
+}
